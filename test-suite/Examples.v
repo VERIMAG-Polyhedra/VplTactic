4a4145ac1fbbeb2b4ab412bf9dc97c9df6913c0e
Sylvain Boulmé <sylvain.boulme@univ-grenoble-alpes.fr>
2018-05-30 10:39:16 +0200

    Minor modification of Examples.v

--- test-suite/Examples.v
+++ test-suite/Examples.v
@@ -13,19 +13,58 @@ Proof.
   vpl.
 Qed.
 
+Lemma ex_intro_decompose (x: Qc) (f: Qc -> Qc):
+   x <= 1
+-> (f x) < (f 1)
+-> x < 1.
+Proof.
+  intros.
+  (* first call to vpl *)
+  vpl_oracle reducedP.  (* reification + meta-script to generate reducedP *)
+  vpl_compute reducedP. (* computation of reducedP *)
+  vpl_rewrite. (* rewriting of learned equalities *)
+  (* second call to vpl *)
+  vpl_oracle reducedP.  (* reification + meta-script to generate reducedP *)
+  vpl_compute reducedP. (* computation of reducedP *)
+  trivial.
+Qed.
 
-Lemma ex_script (x1 x2 x3: Qc):
--(1 # 2) * x2 >= x1
+
+
+Lemma ex_script (A:Type) (f: Qc -> A) (x1 x2 x3: Qc):
+f ((1 + 2*x1*x2)/(1-2*x3)) <> f (1+x2)
+-> -(1 # 2) * x2 >= x1
 -> 2*x3 >= x2
 -> 3*x1 >= x2
 -> x1 >= -10
--> x1 + x2 >= x3
--> False.
+-> x1 + x2 < x3.
 Proof.
-  vpl_grab.
-  vpl_oracle a.
-  vpl_compute a.
-Abort.
+  vpl_auto.
+Qed.
+
+
+Lemma ex_script_decompose (A:Type) (f: Qc -> A) (x1 x2 x3: Qc):
+f ((1 + 2*x1*x2)/(1-2*x3)) <> f (1+x2)
+-> x1 <= -(1 # 2) * x2
+-> x2 <= 2*x3
+-> x2 <= 3*x1
+-> -10 <= x1
+-> x1 + x2 < x3.
+Proof.
+  intros.
+  (* first call to vpl *)
+  vpl_oracle rP.  (* reification + meta-script to generate rP *)
+  vpl_compute rP. (* computation of reducedP *)
+  vpl_rewrite. (* rewriting of learned equalities *)
+  (* end of the first call to vpl *)
+  intros; apply H.
+  apply f_equal.
+  field.
+  (* second call to vpl *)
+  vpl_oracle rP.
+  vpl_compute rP. 
+  trivial.
+Qed.
 
 
 Lemma bigex (A:Type) (f:A -> Qc) (g:Qc -> A) (v1 v2 v3 v4: Qc):
